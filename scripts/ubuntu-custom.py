9666248b6f833f1b847fd1001e51fa0cf022b29f
Marcus Vinicius <marcusviniciuscote@gmail.com>
2024-10-21 17:46:18 -0300

    docker custom ubuntu

--- scripts/ubuntu-custom.py
+++ scripts/ubuntu-custom.py
@@ -22,12 +22,21 @@ def aliases_comand(alias, comand):
 
 update_and_upgrade = "sudo apt update && sudo apt upgrade -y && sudo apt autoremove"
 shutdown = "shutdown -h now"
-install_some_programs = "sudo apt install -y neofetch vim neovim virtualbox virtualbox-guest-additions-iso virtualbox-guest-utils eza bat zsh zsh-autosuggestions zsh-syntax-highlighting htop curl docker-ce docker-ce-cli containerd.io docker-compose-plugin"
+install_some_programs = "sudo apt install -y neofetch vim neovim virtualbox virtualbox-guest-additions-iso virtualbox-guest-utils eza bat zsh zsh-autosuggestions zsh-syntax-highlighting htop ca-certificates curl docker-ce docker-ce-cli containerd.io docker-compose-plugin"
+
 install_zsh = "sh -c '$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'"
+
+docker_dependencies = f'echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
+sudo apt-get update'
+
+install_docker = "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+
 
 terminal_comand(update_and_upgrade)
 terminal_comand(install_some_programs)
 terminal_comand(install_zsh)
+terminal_comand(docker_dependencies)
+terminal_comand(install_docker)
 
 #setting zsh is default
 default_zsh = "chsh -s /usr/bin/zsh"
