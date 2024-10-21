d854298114a1b3d57a49137997b2401d5cb16fad
Marcus Vinicius <marcusviniciuscote@gmail.com>
2024-10-21 19:47:48 -0300

    install nerd-font

--- scripts/ubuntu-custom.py
+++ scripts/ubuntu-custom.py
@@ -47,7 +47,6 @@ test_docker = "docker run hello-world"
 terminal_comand(update_and_upgrade)
 terminal_comand(install_some_programs)
 terminal_comand(install_zsh)
-terminal_comand(default_zsh)
 terminal_comand(install_nerd_font)
 terminal_comand(docker_down)
 terminal_comand(docker_dependencies)
@@ -55,6 +54,7 @@ terminal_comand(install_docker)
 terminal_comand(docker_post_install)
 terminal_comand(test_docker)
 terminal_comand(docker_start_on_boot)
+terminal_comand(default_zsh)
 
 #adding aliases
 aliases_comand('ls', 'eza --icons')
